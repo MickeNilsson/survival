a3540020a40e5f4ceab9e0e755d4590e99a326e6
Mikael Nilsson <mikaelnilsson@hotmail.com>
2018-02-11 19:33:52 +0100

    Add image table

--- editor/js/index.js
+++ editor/js/index.js
@@ -1,5 +1,6 @@
 (function($){
   $('document').ready(function(){
+    var tileXY_s;
     $('#create-level').on('click', function(){
       var numOfTilesX_i = parseInt($('#x-tiles').val());
       var numOfTilesY_i = parseInt($('#y-tiles').val());
@@ -13,12 +14,32 @@
       for(y = 0; y < numOfTilesY_i; y += 1){
         level_s += '<tr>';
         for(x = 0; x < numOfTilesX_i; x += 1){
-          level_s += '<td>0</td>';
+          level_s += '<td data-xy="' + x + '' + y + '" data-y="' + y + '" data-x="' + x + '"><img data-xy="' + x + '' + y + '" src="../assets/images/icon01.png" /></td>';
         }
         level_s += '</tr>';
       }
       $('#level tbody').empty();
       $('#level tbody').append(level_s);
+    });
+
+    $('#level tbody').on('click', function(e){
+      console.dir(e);
+      tileXY_s = e.target.dataset.xy;
+      $('#tile-modal').modal('show');
+    });
+
+    $('#tile-modal tbody').on('click', function(e){
+      console.dir(e.target);
+      var td_s = 'td[data-xy=' + tileXY_s + '] img';
+      var srcValue_s;
+      if(e.target.src){
+        srcValue_s = e.target.src;
+      }else{
+        srcValue_s = e.target.firstChild.src;
+      }
+      console.log(td_s);
+      $(td_s).attr('src', srcValue_s);
+
     });
   });
 }(jQuery));
