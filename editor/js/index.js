969f6c207ce175160b8adc35caa14a1d88485ee6
Mikael Nilsson <mikaelnilsson@hotmail.com>
2018-04-29 19:26:04 +0200

    Refactor

--- editor/js/index.js
+++ editor/js/index.js
@@ -1,195 +1,184 @@
 (function($){
 
-  $('document').ready(function(){
+$('document').ready(function(){
+	var tileXY_s;
+	var matrix = [];
+	var numOfTilesX_i;
+	var numOfTilesY_i;
+	var mapTile_o = {id:null, x:null, y:null};
+	var savedLevels_a = {};
+	var maxTilesPerRow_i = 4;
 
-    var tileXY_s;
-    var matrix = [];
-    var numOfTilesX_i;
-    var numOfTilesY_i;
-    var savedLevels_a = {};
+	// Download the textureatlas json file and create a <tbody> using its data.
+	$.get('../assets/images/textureatlas.json', function(textureatlas){
+		if(typeof textureatlas === 'string'){
+			textureatlas = JSON.parse(textureatlas);
+		}
+		var mapTilesRow_o = $('<tr></tr>');
+		$.each(textureatlas.frames, function(imageFileName_s){
+			var mapTile_o = $('<td><img src="../assets/images/' + imageFileName_s + '" /></td>');
+			mapTilesRow_o.append(mapTile_o);
+			var numOfTilesInCurrentRow_i = mapTilesRow_o[0]['children']['length'];
+			if(numOfTilesInCurrentRow_i === maxTilesPerRow_i){
+				$('#tile-modal tbody').append(mapTilesRow_o);
+				mapTilesRow_o = $('<tr></tr>');
+			}
+		});
+		$('#tile-modal tbody').append(mapTilesRow_o);
+	});
 
-    // Add all images to the modal.
-    $.get('../assets/images/textureatlas.json', function(textureatlas){
-      
-      if(typeof textureatlas === 'string'){
-        textureatlas = JSON.parse(textureatlas);
-      }
-      var images_s = '<tr>';
-      var image_i = 0;
-      for(var imageFilename_s in textureatlas.frames){
-        if(textureatlas.frames.hasOwnProperty(imageFilename_s)){
-          images_s += '<td><img src="../assets/images/' + imageFilename_s + '" /></td>';
-          image_i += 1;
-          if(image_i > 4){
-            images_s += '</tr><tr>';
-            image_i = 0;
-          }
-        }
-        
-      }
-      images_s += '</tr>';
-      $('#tile-modal tbody').append(images_s);
-      console.log(images_s);
-    });
+	// Get all saved levels.
+	$.get('http://www.digizone.se/survival/levels/get_levels.php')
+		.done(function(levels_o){
+			var savedLevels_a = [];
+			$.each(levels_o, function(levelName_s, level_a){
+				savedLevels_a[levelName_s] = JSON.parse(level_a);
+				$('#saved-levels-ul').append('<li>' + decodeURIComponent(levelName_s) + '</li>');
+			});
+		});
 
-    // Get all saved levels.
-    $.get('http://www.digizone.se/survival/levels/get_levels.php')
-      .done(function(response){
-        console.dir(response);
-        for(var level_s in response){
-          if(response.hasOwnProperty(level_s)){
-            var levelName_s = level_s.substring(0, level_s.indexOf('.json'));
-            savedLevels_a[level_s] = JSON.parse(response[level_s]);
-            $('#saved-levels-ul').append('<li>' + decodeURIComponent(levelName_s) + '</li>');
-          }
-        }
-        console.log('savedLevels_a: ');
-        console.dir(savedLevels_a);
-      });
+	// Load saved level into matrix.
+	$('#saved-levels-ul').on('click', 'li', function(e){
+		e.stopPropagation();
+		console.dir(e);
+	});
 
-    $('#saved-levels-ul').on('click', 'li', function(e){
-      e.stopPropagation();
-      console.dir(e);
-    });
+	// Create a new empty matrix when the user clicks the create level button.
+	$('#create-level-button').on('click', function(){
+		numOfTilesX_i = +$('#num-of-x-tiles').val();
+		numOfTilesY_i = +$('#num-of-y-tiles').val();
+		var level_a = new Array(numOfTilesX_i);
+		var levelRow_o;
+		$.each(level_a, function(x){
+			level_a[x] = new Array(numOfTilesY_i);
+		});
+		$('#level tbody').empty();
+		for(var y = 0; y < numOfTilesY_i; y += 1){
+			levelRow_o = $('<tr></tr>');
+			for(var x = 0; x < numOfTilesX_i; x += 1){
+				levelRow_o.append('<td id="' + x + ':' + y + '" data-x="' + x + '" data-y="' + y + '" ><img src="../assets/images/grass01.png" /></td>');
+			}
+			$('#level tbody').append(levelRow_o);
+		}
+	});
 
+	// Show the tile modal when the user clicks a tile in the matrix.
+	$('#level tbody').on('click', function(e){
+		mapTile_o.x = e.target.parentElement.dataset.x;
+		mapTile_o.y = e.target.parentElement.dataset.y;
+		mapTile_o.id = e.target.parentElement.id;
+		console.dir(mapTile_o);
+		$('#tile-modal').modal('show');
+	});
 
-    // Create a new empty matrix when the user clicks the create button.
-    $('#create-level').on('click', function(){
-      numOfTilesX_i = parseInt($('#x-tiles').val());
-      numOfTilesY_i = parseInt($('#y-tiles').val());
-      var level_a = new Array(numOfTilesX_i);
-      var x;
-      var y;
-      var level_s = '';
-      for(x = 0; x < numOfTilesX_i; x += 1){
-        level_a[x] = new Array(numOfTilesY_i);
-      }
-      for(y = 0; y < numOfTilesY_i; y += 1){
-        level_s += '<tr>';
-        for(x = 0; x < numOfTilesX_i; x += 1){
-          level_s += '<td data-xy="' + x + '' + y + '" data-y="' + y + '" data-x="' + x + '"><img data-xy="' + x + '' + y + '" src="../assets/images/grass01.png" /></td>';
-        }
-        level_s += '</tr>';
-      }
-      $('#level tbody').empty();
-      $('#level tbody').append(level_s);
-    });
-
-    // Show the tile modal when the user clicks a tile in the matrix.
-    $('#level tbody').on('click', function(e){
-      console.dir(e);
-      tileXY_s = e.target.dataset.xy;
-      $('#tile-modal').modal('show');
-    });
-
-    // Save the matrix in a JSON-object.
-    $('#save-level').on('click',function(){
-      var y = 0;
-      //console.log('antal td');
-      //console.dir($('#level tbody tr'));
-      $('#level tbody tr').each(function(index, td){
-        console.dir(td);
-        console.log('index: ' + index);
-        
-        var temp_a = [];
-        for(var i = 0; i < td.children.length; i += 1){
-          var indexOfLastSlash_i = td.children[i].children[0].src.lastIndexOf('/');
-          var indexOfDotPng_i = td.children[i].children[0].src.indexOf('.png');
-          var imageFilename_s = td.children[i].children[0].src.substring(indexOfLastSlash_i + 1, indexOfDotPng_i);
-          temp_a.push(imageFilename_s);
-        }
-        matrix.push(temp_a);
-      });
-      console.dir(matrix);
-      window.a = matrix;
-      console.dir(JSON.stringify(matrix));
-      $('#json').text(JSON.stringify(matrix));
-      // At this point the x and y coordinates of the array is inverted i.e. matrix[y][x].
-      // Convert the array so that matrix[x][y].
-      var maxX_i = matrix[0].length;
-      var maxY_i = matrix.length;
-      var tempMatrix_a = new Array(maxX_i);
-      for(i = 0; i < maxX_i; i++){
-        tempMatrix_a[i] = new Array(maxY_i);
-      }
-      for(var x = 0; x < maxX_i; x++){
-        for(var y = 0; y < maxY_i; y++){
-          tempMatrix_a[x][y] = matrix[y][x];
-        }
-      }
-      console.log('tempMatrix');
-      console.dir(tempMatrix_a);
-      window.a = tempMatrix_a;
+	// Save the matrix in a JSON-object.
+	$('#save-level').on('click',function(){
+	var y = 0;
+	//console.log('antal td');
+	//console.dir($('#level tbody tr'));
+	$('#level tbody tr').each(function(index, td){
+		console.dir(td);
+		console.log('index: ' + index);
+		
+		var temp_a = [];
+		for(var i = 0; i < td.children.length; i += 1){
+			var indexOfLastSlash_i = td.children[i].children[0].src.lastIndexOf('/');
+			var indexOfDotPng_i = td.children[i].children[0].src.indexOf('.png');
+			var imageFilename_s = td.children[i].children[0].src.substring(indexOfLastSlash_i + 1, indexOfDotPng_i);
+			temp_a.push(imageFilename_s);
+		}
+		matrix.push(temp_a);
+	});
+	console.dir(matrix);
+	window.a = matrix;
+	console.dir(JSON.stringify(matrix));
+	$('#json').text(JSON.stringify(matrix));
+	// At this point the x and y coordinates of the array is inverted i.e. matrix[y][x].
+	// Convert the array so that matrix[x][y].
+	var maxX_i = matrix[0].length;
+	var maxY_i = matrix.length;
+	var tempMatrix_a = new Array(maxX_i);
+	for(i = 0; i < maxX_i; i++){
+		tempMatrix_a[i] = new Array(maxY_i);
+	}
+	for(var x = 0; x < maxX_i; x++){
+		for(var y = 0; y < maxY_i; y++){
+			tempMatrix_a[x][y] = matrix[y][x];
+		}
+	}
+	console.log('tempMatrix');
+	console.dir(tempMatrix_a);
+	window.a = tempMatrix_a;
 
 
-    });
+	});
 
-    // If a tile has been clicked in the tile modal, set the tile in the matrix
-    // to this tile.
-    $('#tile-modal tbody').on('click', function(e){
-      console.dir(e.target);
-      var td_s = 'td[data-xy=' + tileXY_s + '] img';
-      var srcValue_s;
-      if(e.target.src){
-        srcValue_s = e.target.src;
-      }else{
-        srcValue_s = e.target.firstChild.src;
-      }
-      console.log(td_s);
-      $(td_s).attr('src', srcValue_s);
-      $('#tile-modal').modal('hide');
-    });
+	// If a tile has been clicked in the tile modal, set the tile in the matrix
+	// to this tile.
+	$('#tile-modal tbody').on('click', function(e){
+	console.dir(e.target);
+	var td_s = 'td[data-xy=' + tileXY_s + '] img';
+	var srcValue_s;
+	if(e.target.src){
+		srcValue_s = e.target.src;
+	}else{
+		srcValue_s = e.target.firstChild.src;
+	}
+	console.log(td_s);
+	$(td_s).attr('src', srcValue_s);
+	$('#tile-modal').modal('hide');
+	});
 
-    // Open save level modal.
-    $('#show-save-level-modal-button').on('click', function(){
+	// Open save level modal.
+	$('#show-save-level-modal-button').on('click', function(){
 
-      $('#save-level-modal').modal('show');
+	$('#save-level-modal').modal('show');
 
-    });
+	});
 
-    // Save level on server as json.
-    $('#save-level-button').on('click', function(){
-      
-      $('#level tbody tr').each(function(index, td){
+	// Save level on server as json.
+	$('#save-level-button').on('click', function(){
+	
+	$('#level tbody tr').each(function(index, td){
 
-        var temp_a = [];
-        for(var i = 0; i < td.children.length; i += 1){
-          var indexOfLastSlash_i = td.children[i].children[0].src.lastIndexOf('/');
-          var indexOfDotPng_i = td.children[i].children[0].src.indexOf('.png');
-          var imageFilename_s = td.children[i].children[0].src.substring(indexOfLastSlash_i + 1, indexOfDotPng_i);
-          temp_a.push(imageFilename_s);
-        }
-        matrix.push(temp_a);
-      });
+		var temp_a = [];
+		for(var i = 0; i < td.children.length; i += 1){
+			var indexOfLastSlash_i = td.children[i].children[0].src.lastIndexOf('/');
+			var indexOfDotPng_i = td.children[i].children[0].src.indexOf('.png');
+			var imageFilename_s = td.children[i].children[0].src.substring(indexOfLastSlash_i + 1, indexOfDotPng_i);
+			temp_a.push(imageFilename_s);
+		}
+		matrix.push(temp_a);
+	});
 
-      $('#json').text(JSON.stringify(matrix));
-      // At this point the x and y coordinates of the array is inverted i.e. matrix[y][x].
-      // Convert the array so that matrix[x][y].
-      var maxX_i = matrix[0].length;
-      var maxY_i = matrix.length;
-      var tempMatrix_a = new Array(maxX_i);
-      for(i = 0; i < maxX_i; i++){
-        tempMatrix_a[i] = new Array(maxY_i);
-      }
-      for(var x = 0; x < maxX_i; x++){
-        for(var y = 0; y < maxY_i; y++){
-          tempMatrix_a[x][y] = matrix[y][x];
-        }
-      }
-      console.log('tempMatrix');
-      console.dir(tempMatrix_a);
-      window.a = tempMatrix_a;
-      var data_o = {
-        'name': encodeURIComponent($('#level-name').val()),
-        'level': JSON.stringify(tempMatrix_a)
-      };
-      console.dir(data_o);
-      $.get('http://www.digizone.se/survival/levels/save_level.php', data_o)
-        .done(function(response){
-          console.log(response);
-        });
-      $('#save-level-modal').modal('hide');
-    });
+	$('#json').text(JSON.stringify(matrix));
+	// At this point the x and y coordinates of the array is inverted i.e. matrix[y][x].
+	// Convert the array so that matrix[x][y].
+	var maxX_i = matrix[0].length;
+	var maxY_i = matrix.length;
+	var tempMatrix_a = new Array(maxX_i);
+	for(i = 0; i < maxX_i; i++){
+		tempMatrix_a[i] = new Array(maxY_i);
+	}
+	for(var x = 0; x < maxX_i; x++){
+		for(var y = 0; y < maxY_i; y++){
+			tempMatrix_a[x][y] = matrix[y][x];
+		}
+	}
+	console.log('tempMatrix');
+	console.dir(tempMatrix_a);
+	window.a = tempMatrix_a;
+	var data_o = {
+		'name': encodeURIComponent($('#level-name').val()),
+		'level': JSON.stringify(tempMatrix_a)
+	};
+	console.dir(data_o);
+	$.get('http://www.digizone.se/survival/levels/save_level.php', data_o)
+		.done(function(response){
+			console.log(response);
+		});
+	$('#save-level-modal').modal('hide');
+	});
+	});
 
-  });
 }(jQuery));
--- editor/php/get_levels.php
+++ editor/php/get_levels.php
@@ -5,14 +5,15 @@ header('Content-type: application/json');
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-//print_r(glob('*.json'))
 
-// Get all filenames ending with .json in the current directory.
-$levelFileNames_a = glob('http://www.digizone.se/survival/levels/*.json');
+$fileNames_a = scandir('./');
 $levels_a = array();
-foreach($levelFileNames_a as $fileName_s){
-  $level_s = file_get_contents($fileName_s);
-  $levels_a[$fileName_s] = $level_s;
+foreach($fileNames_a as $fileName_s){
+  if(strpos($fileName_s, '.json') !== false){
+    $level_s = file_get_contents($fileName_s);
+    $levelName_s = substr($fileName_s, 0, -5);
+    $levels_a[$levelName_s] = $level_s;
+  }
 }
 echo json_encode($levels_a, JSON_UNESCAPED_UNICODE)
 
