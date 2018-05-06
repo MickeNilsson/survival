1102d83063f1eab38ebbdfa248efe21ea698dcc6
Mikael Nilsson <mikaelnilsson@hotmail.com>
2018-05-06 18:08:55 +0200

    Make it possible to save an existing level

--- editor/js/index.js
+++ editor/js/index.js
@@ -10,6 +10,7 @@ $('document').ready(function(){
 	var maxTilesPerRow_i = 4;
 	var currentMapTileId_s = '';
 	var level_aa = [];
+	var levelName_s;
 
 	// Download the textureatlas json file and create a <tbody> using its data.
 	$.get('../assets/images/textureatlas.json', function(textureatlas){
@@ -34,11 +35,27 @@ $('document').ready(function(){
 	// Load saved level into matrix.
 	$('#saved-levels-ul').on('click', 'li', function(e){
 		e.stopPropagation();
-		console.dir(savedLevels_a);
+		levelName_s = encodeURIComponent(e.target.textContent);
+		level_aa = savedLevels_a[levelName_s];
+		levelName_s = decodeURIComponent(levelName_s);
+		numOfTilesX_i = level_aa.length;
+		numOfTilesY_i = level_aa[0].length;
+		console.log(levelName_s);
+		//console.log(e.target.textContent);
+		console.dir(level_aa);
+		$('#level tbody').empty();
+		for(var y = 0; y < numOfTilesY_i; y += 1){
+			levelRow_o = $('<tr></tr>');
+			for(var x = 0; x < numOfTilesX_i; x += 1){
+				levelRow_o.append('<td id="' + x + ':' + y + '"><img src="../assets/images/' + level_aa[x][y] + '.png" /></td>');
+			}
+			$('#level tbody').append(levelRow_o);
+		}
 	});
 
 	// Create new level with only grass tiles.
 	$('#create-level-button').on('click', function(){
+		levelName_s = '';
 		numOfTilesX_i = +$('#num-of-x-tiles').val();
 		numOfTilesY_i = +$('#num-of-y-tiles').val();
 		var levelRow_o;
@@ -75,26 +92,33 @@ $('document').ready(function(){
 
 	// Open "save level"-modal.
 	$('#show-save-level-modal-button').on('click', function(){
-		$('#save-level-status').html('');
-		$('#level-name').val('');
-		$('#save-level-form').show();
-		$('#save-level-modal').modal('show');
+		if(levelName_s){
+			saveLevel();
+		}else{
+			$('#save-level-status').html('');
+			$('#level-name').val('');
+			$('#save-level-form').show();
+			$('#save-level-modal').modal('show');
+		}
 	});
 
 	// Save level on server as json.
 	$('#save-level-button').on('click', function(){
-			var newLevel_o = {
-				name: $('#level-name').val(),
-				level: JSON.stringify(level_aa)
-			};
-			$('#save-level-form').hide();
-			$('#save-level-status').html('Saving level...');
-			$.get('http://www.digizone.se/survival/levels/save_level.php', newLevel_o)
-				.done(function(response){
-					$('#save-level-status').html(response.status);
-				});
-			//$('#save-level-modal').modal('hide');
-		});
+		saveLevel();
 	});
 
+	function saveLevel(){
+		var newLevel_o = {
+			name: levelName_s || $('#level-name').val(),
+			level: JSON.stringify(level_aa)
+		};
+		$('#save-level-form').hide();
+		$('#save-level-status').html('Saving level...');
+		$.get('http://www.digizone.se/survival/levels/save_level.php', newLevel_o)
+			.done(function(response){
+				$('#save-level-status').html(response.status);
+				$('#save-level-modal').modal('show');
+			});
+	}
+});
 }(jQuery));
